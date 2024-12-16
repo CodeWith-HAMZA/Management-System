dbd2f848efb7b9376e9f4b2a2ab6fc72348922be
Hamza <hamza263973@gmail.com>
2024-12-16 23:18:32 +0500

    init

---
+++ remix-app/app/routes/_index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '~/components/ui/button'
+
+const Index = () => {
+  return (
+    <div>
+      <Button>Shadud</Button>
+    </div>
+  )
+}
+
+export default Index
